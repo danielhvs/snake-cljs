054f86bf08bdc78e2e3dd1d20f6e64757bc2d527
Daniel Habib <daniel.habib@dataprev.gov.br>
2021-09-20 18:11:50 -0300

    mapa draw

--- src/snake_cljs/core.cljs
+++ src/snake_cljs/core.cljs
@@ -4,15 +4,41 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
+(def w-max 20)
+(def h-max 20)
+(def tam-ret 15)
+
 (defonce app-state (atom {:text "Hello world!"}))
 
-(defn hello-world []
+(defn retangulo [x y cor]
+  [:rect
+   {:width tam-ret
+    :height tam-ret
+    :fill cor
+    :x x
+    :y y}])
+
+(defn ->retangulo [x y cor]
+  (retangulo (* x tam-ret) (* y tam-ret) cor))
+
+(defn fundo []
+  (into []
+        (for [x (range 0 30)
+              y (range 0 30)]
+          (->retangulo x y "gray"))))
+
+(defn snake-world []
   [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Oi, jovem!"]])
+   (into
+     [:svg
+      (let [w 300 h 300]
+        {:view-box (str "0 0 " w " " h)
+         :width w
+         :height h})]
+     (fundo))])
 
 (defn start []
-  (reagent/render-component [hello-world]
+  (reagent/render-component [snake-world]
                             (. js/document (getElementById "app"))))
 
 (defn ^:export init []
@@ -25,5 +51,3 @@
   ;; stop is called before any code is reloaded
   ;; this is controlled by :before-load in the config
   (js/console.log "stop"))
-
-+
